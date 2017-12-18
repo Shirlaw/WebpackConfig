825b249ae9c231a38bfaeb2ac2a5244d0ec16070
James Shirlaw <jamespshirlaw@gmail.com>
2017-12-17 20:25:10 -0800

    cleanup dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry:
@@ -19,5 +19,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  profile: true,
 };
