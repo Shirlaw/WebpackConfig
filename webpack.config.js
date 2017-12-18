22d01794e6ac6f56e008399ca6ee95098b594d8e
James Shirlaw <jamespshirlaw@gmail.com>
2017-12-17 20:18:13 -0800

    multiple output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry:
+  {
+    index: './src/index.js',
+    project: './src/project.js'
+
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Webpack Config'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 };
